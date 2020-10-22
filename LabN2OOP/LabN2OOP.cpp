6e02e47a4e248ca716533df7ac4f903b51d29836
MeRkyLeZ <robert.avhadiev.01@mail.ru>
2020-10-22 18:36:14 +0500

    Update LabN2OOP.cpp

--- LabN2OOP/LabN2OOP.cpp
+++ LabN2OOP/LabN2OOP.cpp
@@ -1,20 +1,103 @@
-﻿// LabN2OOP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+class Point {	// Интерфейс
+public:
+	virtual void get() = NULL;	// Виртуальный метод вывода
+
+	Point() {	// Конструктор
+		cout << "Point construct\n";
+	}
+	~Point() {	// Деструктор
+		cout << "Point destruct\n";
+	}
+};
+
+class Point2D : public Point {	// Точка в 2-х мерном пространстве
+public:
+	int x;
+	int y;
+
+	Point2D() {	// Конструктор
+		x = 0;
+		y = 0;
+		cout << "Point2D construct\n";
+	}
+	Point2D(int x, int y) {	// Конструктор
+		this->x = x;
+		this->y = y;
+		cout << "Point2D construct\n";
+	}
+	Point2D(const Point2D& p) {	// Конструктор копирования
+		x = p.x;
+		y = p.y;
+		cout << "Point2D construct copy\n";
+	}
+	~Point2D() {	// Деструктор
+		cout << "Point2D destruct\n";
+	}
+	void set(int x, int y) {	// Установка значений
+		this->x = x;
+		this->y = y;
+	}
+	void get() {	// Вывод
+		cout << "x=" << this->x << " , y=" << this->y << "\n";
+	}
+	void setX(int x) {	// Утановка x
+		this->x = x;
+	}
+	void setY(int y) {	// Утановка y
+		this->y = y;
+	}
+	int getX() {	// Получение x
+		return x;
+	}
+	int getY() {	// Получение y
+		return y;
+	}
+};
+
+class Point3D : public Point2D {
+public:
+	int z;
+	Point3D() {	// Конструктор
+		z = 0;
+		cout << "Point3D construct\n";
+	}
+	Point3D(int x, int y, int z) {	// Конструктор
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		cout << "Point3D construct\n";
+	}
+	Point3D(const Point3D& p) {	// Конструктор копирования
+		x = p.x;
+		y = p.y;
+		z = p.z;
+		cout << "Point3D construct copy\n";
+	}
+	~Point3D() {	// Деструктор
+		cout << "Point3D destruct\n";
+	}
+	void set(int x, int y, int z) {	// Установка значений
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	void get() {	// Вывод
+		cout << "x=" << this->x << " , y=" << this->y << " , z=" << this->z << "\n";
+	}
+	void setZ(int z) {	// Утановка z
+		this->z = z;
+	}
+	int getZ() {	// Получение z
+		return z;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
