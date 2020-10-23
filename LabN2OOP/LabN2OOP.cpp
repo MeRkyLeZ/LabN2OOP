5ac73995c52d948653a9716fd62f183cc27d7345
MeRkyLeZ <robert.avhadiev.01@mail.ru>
2020-10-23 19:39:33 +0500

    Update LabN2OOP.cpp

--- LabN2OOP/LabN2OOP.cpp
+++ LabN2OOP/LabN2OOP.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 class Point {	// Интерфейс
-public:
+protected:
 	virtual void get() = NULL;	// Виртуальный метод вывода
 
+public:
 	Point() {	// Конструктор
 		cout << "Point construct\n";
 	}
@@ -15,16 +16,17 @@ public:
 };
 
 class Point2D : public Point {	// Точка в 2-х мерном пространстве
-public:
-	int x;
-	int y;
+protected:
+	double x;
+	double y;
 
+public:
 	Point2D() {	// Конструктор
 		x = 0;
 		y = 0;
 		cout << "Point2D construct\n";
 	}
-	Point2D(int x, int y) {	// Конструктор
+	Point2D(double x, double y) {	// Конструктор
 		this->x = x;
 		this->y = y;
 		cout << "Point2D construct\n";
@@ -37,35 +39,41 @@ public:
 	~Point2D() {	// Деструктор
 		cout << "Point2D destruct\n";
 	}
-	void set(int x, int y) {	// Установка значений
+	void set(double x, double y) {	// Установка значений
 		this->x = x;
 		this->y = y;
 	}
 	void get() {	// Вывод
-		cout << "x=" << this->x << " , y=" << this->y << "\n";
+		cout << "x=" << this->x << " , y=" << this->y << " , dist=" << getdist();
 	}
-	void setX(int x) {	// Утановка x
+	void setX(double x) {	// Утановка x
 		this->x = x;
 	}
-	void setY(int y) {	// Утановка y
+	void setY(double y) {	// Утановка y
 		this->y = y;
 	}
-	int getX() {	// Получение x
+	double getX() {	// Получение x
 		return x;
 	}
-	int getY() {	// Получение y
+	double getY() {	// Получение y
 		return y;
+	}
+private:
+	double getdist() {	// Получение расстояния до центра
+		return sqrt(x * x + y * y);
 	}
 };
 
 class Point3D : public Point2D {
+protected:
+	double z;
+
 public:
-	int z;
 	Point3D() {	// Конструктор
 		z = 0;
 		cout << "Point3D construct\n";
 	}
-	Point3D(int x, int y, int z) {	// Конструктор
+	Point3D(double x, double y, double z) {	// Конструктор
 		this->x = x;
 		this->y = y;
 		this->z = z;
@@ -80,19 +88,23 @@ public:
 	~Point3D() {	// Деструктор
 		cout << "Point3D destruct\n";
 	}
-	void set(int x, int y, int z) {	// Установка значений
+	void set(double x, double y, double z) {	// Установка значений
 		this->x = x;
 		this->y = y;
 		this->z = z;
 	}
 	void get() {	// Вывод
-		cout << "x=" << this->x << " , y=" << this->y << " , z=" << this->z << "\n";
+		cout << "x=" << this->x << " , y=" << this->y << " , z=" << this->z << " , dist=" << getdist();
 	}
-	void setZ(int z) {	// Утановка z
+	void setZ(double z) {	// Утановка z
 		this->z = z;
 	}
-	int getZ() {	// Получение z
+	double getZ() {	// Получение z
 		return z;
+	}
+private:
+	double getdist() {	// Получение расстояния до центра
+		return sqrt(x * x + y * y + z * z);
 	}
 };
 
